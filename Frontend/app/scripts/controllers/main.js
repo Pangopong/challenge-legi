2dbc35c3d84094f7ece815f1eb2427ac6afd517c
Lappy\Raziel <e@e.com>
2016-10-22 16:56:45 +0300

    jhkdashj

--- Frontend/app/scripts/controllers/main.js
+++ Frontend/app/scripts/controllers/main.js
@@ -9,7 +9,7 @@
  */
 angular.module('lawApp')
   .controller('MainCtrl', function ($scope, $http) {
-  	$scope.q = 'Search';
+  	$scope.q = null;
     $scope.jsonator = [];
   	//$scope.jsonator = [{name: 'AAR-SP', value: 26}, {name:'Duminica', value: 26}, {name:'Euripide', value: 90}];
 
